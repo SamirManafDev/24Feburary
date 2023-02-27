27182c0f5540a13ba8494863ee5bf10573a20c91
SamirManafDev <samirmanafli161@gmail.com>
2023-02-27 17:37:24 +0400

    24 feburary task

---
+++ task/Task_24feburary/Task_24feburary/Program.cs
@@ -0,0 +1,111 @@
+﻿//Taks 24 Feburary
+
+
+//int num = 5;
+//if (num > 0)
+//{
+//    Console.WriteLine("Eded musbetdir");
+//}
+//else if (num < 0)
+//{
+//    Console.WriteLine("Eded menfidir");
+//}
+//else
+//{
+//    Console.WriteLine("0 ne menfi,ne de musbetdir");
+//}
+
+
+
+
+
+
+
+//using System.ComponentModel.Design;
+
+//int num = 32;
+//if (num % 2 == 0)
+//{
+//    Console.WriteLine("Eded cutdur");
+//}
+//else if (num % 2 == 1)
+//{
+//    Console.WriteLine("Eded tekdir");
+//}
+
+
+
+
+
+
+
+
+
+
+//int year = 2005;
+//year += 18;
+//if (year > 2023)
+//{
+//    Console.WriteLine("18 yasiniz tamam olmayib");
+//}
+//else if (year <= 2023) 
+//{
+//    Console.WriteLine("18 yasiniz tamam olub");
+//}
+
+
+
+
+
+
+
+
+
+
+//int num1 = 3;
+//int num2 = 7;
+//int num3 = 62;
+//int sum = 0;
+
+//{
+//    if (num1 > 5)
+//    {
+//        sum = sum + num1;
+//    }
+//    if (num2 > 5)
+//    {
+//        sum = sum + num2;
+//    }
+//    if (num3 > 5)
+//    {
+//        sum = sum + num3;
+
+//        Console.WriteLine("5-den boyuk ededlerin cemi:" + sum);
+//    }
+
+//    else
+//    {
+//        Console.WriteLine("50den boyuk eded yoxdur");
+//    }
+//}
+
+
+
+
+
+
+
+
+
+
+
+//int num = 528, sum, num1;
+//num1 = num % 100;
+//sum = (num - num1) / 100;
+//Console.WriteLine(sum);
+
+
+
+
+
+
